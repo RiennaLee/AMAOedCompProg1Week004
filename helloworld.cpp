fa52f66aa49b42aa13ad3f237b37287f46fd8194
RiennaLee <riennalee.cantre@oed.com.ph>
2024-06-24 05:37:49 +0800

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() {
+   std::cout << "Hello\n";
+   std::cout << "World\n";
+     return 0;
+}
+ #include <iostream>    
+
+int main() {
+     std::cout << "H";
+     std::cout << "e";
+     std::cout << "l";  
+     std::cout << "l";
+     std::cout << "o"; 
+     std::cout << "\n";
+     std::cout << "W";
+     std::cout << "o";
+     std::cout << "r";
+     std::cout << "l";
+     std::cout << "d\n";
+     return 0;
+} 
+ #include <iostream>
+
+  int main() {
+    const char hello[] = "Hello";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << hello[i];
+}
+      std::cout << std::endl;
+      
+    // Using five cout statements for "World"
+    std::cout << "W";
+    std::cout << "o";
+    std::cout << "r";
+    std::cout << "l";
+    std::cout << "d\n"
+
+    return 0;
+}    
+               
